202ca52b4a3eb491654ac2cf6509e981624ee30a
umair shah <umairshah14@hotmail.com>
2023-03-13 00:18:30 +0000

    home page updated with styling

--- react-portfolio/src/components/Gallery/Project.js
+++ react-portfolio/src/components/Gallery/Project.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 function Project(props) {
   return (
-    <Card style={{ width: "30rem" }}>
+    <Card style={{ width: "25rem" }} className="col-4">
       <Card.Img variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
