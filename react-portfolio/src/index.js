4966ff7f92ea0471fc71fb8caab8a810c1208c88
umair shah <umairshah14@hotmail.com>
2023-04-14 00:04:07 +0100

    add live hosting

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -6,8 +6,6 @@ import { HashRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Router>
 );
