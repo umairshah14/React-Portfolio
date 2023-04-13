21483f58f3c3117cfeea2dd01c45079845b1efea
umair shah <umairshah14@hotmail.com>
2023-04-13 23:46:20 +0100

    add live hosting

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter  as Router,Routes,Route} from "react-router-dom";
+import { HashRouter  as Router,Routes,Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
