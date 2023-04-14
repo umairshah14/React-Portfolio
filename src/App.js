e29e3aa96f4c8ead7a53bb077ad64fc18c345312
umair shah <umairshah14@hotmail.com>
2023-04-14 19:27:07 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Contact from "./components/Contact";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
- 
+
 function App() {
   return (
       <div>
